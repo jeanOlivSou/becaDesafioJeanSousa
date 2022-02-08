62fb4f1c51166b391c729e48ac84a5a42040c62c
Jean Sousa <jean.sousa1230@gmail.com>
2022-02-08 16:58:25 -0300

    Novos tratamentos de exceções

--- src/main/java/io/github/jeanolivsou/JSnackbar/exceptions/ExceptionHandle.java
+++ src/main/java/io/github/jeanolivsou/JSnackbar/exceptions/ExceptionHandle.java
@@ -1,12 +1,15 @@
 package io.github.jeanolivsou.JSnackbar.exceptions;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class ExceptionHandle {
@@ -26,7 +29,52 @@ public class ExceptionHandle {
         defaultException.setDateTime(LocalDateTime.now());
         defaultException.setStatus(HttpStatus.BAD_REQUEST.value());
 
-        return ResponseEntity.status(defaultException.getStatus()).body(defaultException);
+        return ResponseEntity
+                .status(defaultException.getStatus())
+                .body(defaultException);
 
     }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<DefaultException> notFound(EmptyResultDataAccessException e){
+        DefaultException defaultException = new DefaultException();
+
+        defaultException.setMessage("Registro não encontrado!");
+        defaultException.setStatus(HttpStatus.NOT_FOUND.value());
+        defaultException.setDateTime(LocalDateTime.now());
+
+        return ResponseEntity
+                .status(defaultException.getStatus())
+                .body(defaultException);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<DefaultException> notFound(NoSuchElementException e){
+        DefaultException defaultException = new DefaultException();
+
+        defaultException.setMessage("Registro não encontrado na consulta!");
+        defaultException.setStatus(HttpStatus.NOT_FOUND.value());
+        defaultException.setDateTime(LocalDateTime.now());
+
+        return ResponseEntity
+                .status(defaultException.getStatus())
+                .body(defaultException);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<DefaultException> jsonError(HttpMessageNotReadableException e){
+        DefaultException defaultException = new DefaultException();
+
+        defaultException.setMessage("Erro de sintaxe no JSON!");
+        defaultException.setStatus(HttpStatus.BAD_REQUEST.value());
+        defaultException.setDateTime(LocalDateTime.now());
+
+        return ResponseEntity
+                .status(defaultException.getStatus())
+                .body(defaultException);
+    }
+
+    
+
+
 }
