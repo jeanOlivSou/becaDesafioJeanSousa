52247f87cf8892feaec1438ed0843eb4757506f1
Jean Sousa <jean.sousa1230@gmail.com>
2022-02-09 15:13:06 -0300

    Ajustes no código

--- src/main/java/io/github/jeanolivsou/JSnackbar/entities/Pedido.java
+++ src/main/java/io/github/jeanolivsou/JSnackbar/entities/Pedido.java
@@ -5,10 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.PastOrPresent;
-import javax.validation.constraints.Positive;
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -22,7 +19,7 @@ public class Pedido {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String status;
-    private Date dataPedido;
+    private LocalDateTime dataPedido = LocalDateTime.now();
 
     @OneToOne
     private Cliente cliente;
